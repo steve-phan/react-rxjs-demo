7db920119b7ba80ce081e322d42423d8e1619c5f
Steve Phan <phanhaingoc@gmail.com>
2023-04-15 08:43:50 +0200

    DONE: SmartCount component

--- src/components/SmartCounter/SmartCounter.tsx
+++ src/components/SmartCounter/SmartCounter.tsx
@@ -1,21 +1,47 @@
-import { subscribe } from "diagnostics_channel";
-import React, { KeyboardEvent, useEffect, useState } from "react";
-import { filter, fromEvent, map, pluck } from "rxjs";
+import React, { useEffect, useState } from "react";
+import { fromEvent, interval } from "rxjs";
+import { filter, switchMap, takeWhile, tap } from "rxjs/operators";
 
 export const SmartCounter = () => {
   const [activeNum, setActiveNum] = useState(0);
 
   useEffect(() => {
-    const inputNum = document.getElementById("range")!;
-
-    // create enterObservable event
+    const inputNum = document.getElementById("range")! as HTMLInputElement;
+    /**
+     * First, fromEvent(inputNum, "keyup") creates an observable that listens * * to keyup events on the input element with the id "range".
+     * Next, using the `pipe` operator to transform this observable.
+     * `filter` to filter only event `keyup` with `Enter` keyboard
+     */
     const enter$ = fromEvent(inputNum, "keyup").pipe(
       filter((event) => (event as unknown as KeyboardEvent).code === "Enter")
     );
-    enter$.subscribe((subscribe) => {
-      console.log({ subscribe });
-    });
+
+    const counter$ = enter$.pipe(
+      switchMap(() => {
+        const endRange = parseInt(inputNum.value);
+        /**
+         * Every 1s `interval(1000) it'll emits a number, then if(number < endRange) `takeWhile` it'll completes the observable sequence and unsubscribed
+         */
+        return interval(1000).pipe(
+          takeWhile((val) => val <= endRange),
+          /**
+           * `tap` is similar to forEach in vanilla JavaScript in that it *  *  * performs a side effect for each value emitted by the observable,
+           *
+           * It's important to note that tap does not modify the emitted values * in any way, whereas operators like map and filter do modify the
+           * emitted values.
+           */
+          tap((val) => setActiveNum(val))
+        );
+      })
+    );
+
+    const counterSubscription = counter$.subscribe();
+
+    return () => {
+      counterSubscription.unsubscribe();
+    };
   }, []);
+
   return (
     <div className="container">
       <input
